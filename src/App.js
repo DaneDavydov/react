24f20efef0f393f21b862659ebab75a666ffee3b
Denis Davydov <dane.tno@gmail.com>
2020-09-01 19:57:05 +0400

    react test

--- src/App.js
+++ src/App.js
@@ -5,65 +5,79 @@ import {
   Route,
   Link
 } from 'react-router-dom';
-
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
-
 import Message from './components/Message';
 
-function App() {
-  const data = [
-    { id: 1, name: 'Денис', logo, title: 'Заголовок', text: 'Hello World!!!' },
-    { id: 2, name: 'Дима', logo, title: 'Заголовок Дмитрия', text: 'Hello World!!! от Димы' },
-    { id: 3, name: 'Дане', logo, title: 'Заголовок Дане', text: 'Hello World!!! Данееее' }
-  ];
+const API = 'https://randomuser.me/api/?results=10';
 
-  return (
-    <Router>
-      <nav className="navigation">
-        <ul className="navigation-list">
-          <li className="navigation-link">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li className="navigation-link">
-            <Link to="/hello">
-              Home
-            </Link>
-          </li>
-          <li className="navigation-link">
-            <Link to="/signin">
-              Home
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route patch="/" exact>
-          <div className="App">
-            {data.map(({name, logo, title, text, id}) => {
-              return (
-                <Message
-                  name={name}
-                  logo={logo}
-                  title={title}
-                  text={text}
-                  key={id}
-                />
-              );
-            })}
-          </div>
-        </Route>
-        <Route path="/hello">
-          Привет!
-        </Route>
-        <Route path="/signin">
-          Я - регистратура!
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userData: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API)
+      .then((res) => {
+        this.setState( { userData: res.data.results });
+      });
+  }
+
+  render() {
+    const { userData } = this.state;
+    return (
+      <Router>
+        <nav className="navigation">
+          <ul className="navigation-list">
+            <li className="navigation-link">
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li className="navigation-link">
+              <Link to="/hello">
+                Hello
+              </Link>
+            </li>
+            <li className="navigation-link">
+              <Link to="/signin">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <div className="App">
+              {userData.map(({name, picture, location, id}) => {
+                return (
+                  <Message
+                    name={`${name.first} ${name.last}`}
+                    logo={picture.thumbnail}
+                    title={location.country}
+                    text={location.city}
+                    key={id.value}
+                  />
+                );
+              })}
+            </div>
+          </Route>
+          <Route path="/hello">
+            Привет!
+          </Route>
+          <Route path="/signin">
+            Регистрация
+          </Route>
+        </Switch>
+      </Router>
+    );
+ }
+
+
 }
 
 export default App;
